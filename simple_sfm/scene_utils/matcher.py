ba5248a0cd6c2ba404deb005212c33d96662e5eb
pavelsol <pal1rus@gmail.com>
2022-10-04 10:17:25 +0400

    Disable randomness in matcher dataloader.

--- simple_sfm/scene_utils/matcher.py
+++ simple_sfm/scene_utils/matcher.py
@@ -191,7 +191,7 @@ class Matcher(object):
 
         num_keypoints = 0
 
-        image_dataloader = DataLoader(dataset, batch_size=1)
+        image_dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
         for data in image_dataloader:
 
