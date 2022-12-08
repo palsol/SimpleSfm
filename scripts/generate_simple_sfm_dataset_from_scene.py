e1f532ffbe8e9ba0628a3905b1c593a3548a3aed
pavelsol <pal1rus@gmail.com>
2022-12-08 20:46:38 +0400

    fixes

--- scripts/generate_simple_sfm_dataset_from_scene.py
+++ scripts/generate_simple_sfm_dataset_from_scene.py
@@ -71,7 +71,7 @@ def main():
 
     simple_sfm_path = Path(os.path.abspath(__file__)).parent.parent
 
-    modnet_weigths_path = Path(simple_sfm_path, '/weights/modnet_photographic_portrait_matting.torchscript')
+    modnet_weigths_path = Path(simple_sfm_path, 'weights/modnet_photographic_portrait_matting.torchscript')
     # ptf_segm_model_weigths_path = '/home/palsol/projects/SimpleSfm/notebooks/data/mma_multi_211122_l_cuda_0.torchscript.pt'
 
     capture_work_dir = Path(output_dir_path, scene_name)
@@ -109,8 +109,8 @@ def main():
         shutil.copytree(input_data_path, frames_path)
 
     matcher = Matcher(
-        super_point_extractor_weights_path=Path(simple_sfm_path, '/weights/superpoint_v1.pth'),
-        super_glue_weights_path=Path(simple_sfm_path, '/weights/superglue_indoor.pth'),
+        super_point_extractor_weights_path=Path(simple_sfm_path, 'weights/superpoint_v1.pth'),
+        super_glue_weights_path=Path(simple_sfm_path, 'weights/superglue_indoor.pth'),
         nms_radius=opts.nms_radius,
         keypoint_threshold=opts.keypoint_threshold,
         matcher_type='super_glue',
